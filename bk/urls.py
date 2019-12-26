fe0adbe27755a658f9bf2d9ff80e877186e497fb
jessonzhong <jessonzhong@163.com>
2019-12-26 17:14:38 +0800

    commit

--- bk/urls.py
+++ bk/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from blog import views
+from django.views.static import serve
+from django.conf import settings
+from blog import urls as blog_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,4 +27,20 @@ urlpatterns = [
     url(r'^reg/', views.register),
     url(r'^index/', views.index),
 
+    # 以blog开头的url都交给app下面的urls.py来处理
+    url(r'^blog/', include(blog_urls)),
+
+    url(r'^get_valid_img.png/', views.get_valid_img),
+
+    # 极验 获取验证码的url
+    url(r'^pc-geetest/register', views.get_geetest),
+
+    # 用来校验用户名是否被注册的接口
+    url(r'^check_username_exist/$', views.check_username_exist),
+
+    # media相关的路由设置
+    url(r'^media/(?P<path>.*)$', serve, {"document_root": settings.MEDIA_ROOT}),
+
+    url(r'^upload/', views.upload),
+    url(r'^$/', views.index),
 ]
---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from blog import views
+
+urlpatterns = [
+    url(r"backend/add_article/", views.add_article),
+    url(r"up_down/", views.up_down),
+    url(r"comment/", views.comment),
+    url(r"comment_tree/(\d+)/", views.comment_tree),
+
+    url(r'(\w+)/(tag|category|archive)/(.+)/', views.home),  # home(request, username, tag)
+    url(r'(\w+)/article/(\d+)/$', views.article_detail),  # 文章详情 article_detail(request)
+    url(r'(\w+)', views.home),  # home(request,username)
+]
